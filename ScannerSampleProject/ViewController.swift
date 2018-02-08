71bed638a394d00c3c655241530539bfcc40d255
Boris Emorine <boris.emorine@gmail.com>
2018-02-08 10:26:18 +0100

    Initial Commit

---
+++ ScannerSampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScannerSampleProject
+//
+//  Created by Boris Emorine on 2/8/18.
+//  Copyright © 2018 WeTransfer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
