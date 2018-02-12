f6b206f3fd60db3625440b563f03345a1b8deb69
Boris Emorine <boris.emorine@gmail.com>
2018-02-12 10:47:00 +0100

    Reorganize project

---
+++ Scanner/Edit/EditScanViewController.swift
@@ -0,0 +1,47 @@
+//
+//  EditScanViewController.swift
+//  Scanner
+//
+//  Created by Boris Emorine on 2/12/18.
+//  Copyright © 2018 WeTransfer. All rights reserved.
+//
+
+import UIKit
+
+class EditScanViewController: UIViewController {
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.isOpaque = true
+        imageView.backgroundColor = .black
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    // MARK: - Setups
+    
+    private func setupViews() {
+        view.addSubview(imageView)
+    }
+    
+    private func setupConstraints() {
+        let constraints = [
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: imageView.bottomAnchor),
+            view.trailingAnchor.constraint(equalTo: imageView.trailingAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+}
