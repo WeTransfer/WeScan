834cd267b23f09c96cc448c640b709e50d453b2b
Boris Emorine <boris.emorine@gmail.com>
2018-02-27 10:58:45 +0100

    Add documentation for ImageScannerControllerDelegate

--- Scanner/ImageScannerController.swift
+++ Scanner/ImageScannerController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import AVFoundation
 
+/// A set of methods that your delegate object must implement to interact with the image scanner interface.
 public protocol ImageScannerControllerDelegate: NSObjectProtocol {
     
     /// Tells the delegate that the user scanned a document.
