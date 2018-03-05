219092b302f184e154b3d1e366e8828c60e85d44
Boris Emorine <boris.emorine@gmail.com>
2018-03-05 17:14:36 +0100

    Add window variable on AppDelegate

--- WeScanSampleProject/AppDelegate.swift
+++ WeScanSampleProject/AppDelegate.swift
@@ -11,6 +11,8 @@ import UIKit
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
