959c8df3867395004c5b0a54547b404d906a846e
Boris Emorine <boris.emorine@gmail.com>
2018-03-07 13:29:38 +0100

    Add documentation to ImageScannerControllerError

--- WeScan/Common/Error.swift
+++ WeScan/Common/Error.swift
@@ -8,11 +8,17 @@
 
 import Foundation
 
+/// Errors related to the `ImageScannerController`
 public enum ImageScannerControllerError: Error {
+    /// The user didn't grant permission to use the camera.
     case authorization
+    /// An error occured when setting up the user's device.
     case inputDevice
+    /// An error occured when trying to capture a picture.
     case capture
+    /// No rectangles have been found.
     case noRectangle
+    /// Error when creating the CIImage.
     case ciImageCreation
 }
 
