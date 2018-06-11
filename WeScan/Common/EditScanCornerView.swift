0db548c04863c858f034794a44714ae2b8e2737d
Boris Emorine <boris.emorine@gmail.com>
2018-06-11 20:14:45 +0200

    Add missing call to super in layoutSubviews()

--- WeScan/Common/EditScanCornerView.swift
+++ WeScan/Common/EditScanCornerView.swift
@@ -38,6 +38,7 @@ final class EditScanCornerView: UIView {
     }
     
     override func layoutSubviews() {
+        super.layoutSubviews()
         layer.cornerRadius = bounds.width / 2.0
     }
     
