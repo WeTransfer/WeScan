73bf3a81a7df18bb67db6949a0414110ff7011dc
James Campbell <james@printtapp.com>
2018-08-11 21:17:18 +0100

    Basic UIImage test.

--- WeScanTests/UIImageTests.swift
+++ WeScanTests/UIImageTests.swift
@@ -29,12 +29,17 @@ class UIImageTests: XCTestCase {
   
   func testScalesCorrectly() {
     
-//    let size = CGSize(width: 1, height: 1)
-//    
-//    UIGraphicsBeginImageContextWithOptions(size, false, 1)
-//    let image = UIGraphicsGetImageFromCurrentImageContext()!
-//    UIGraphicsEndImageContext()
-//    
-//    XCTAssertNil(image.scaledImage(atPoint: .zero, scaleFactor: 5.0, targetSize: .zero))
+    let middle = CGPoint(x: 0.5, y: 0.5)
+    let size = CGSize(width: 1, height: 1)
+    let targetSize = CGSize(width: 2.0, height: 2.0)
+
+    UIGraphicsBeginImageContextWithOptions(size, false, 1)
+    let image = UIGraphicsGetImageFromCurrentImageContext()!
+    UIGraphicsEndImageContext()
+    
+    let scaledImage = image.scaledImage(atPoint: middle, scaleFactor: 2.0, targetSize: targetSize)!
+
+    XCTAssertEqual(scaledImage.size.width, 1.0)
+    XCTAssertEqual(scaledImage.size.height, 1.0)
   }
 }
