a8226ff9c0e46a337fc3a9804a4648ab8104dd03
James Campbell <james@printtapp.com>
2018-08-11 20:40:15 +0100

    CaptureSession tests.

--- WeScanTests/CaptureSessionTests.swift
+++ WeScanTests/CaptureSessionTests.swift
@@ -7,29 +7,18 @@
 //
 
 import XCTest
+@testable import WeScan
 
 class CaptureSessionTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testAutoScanEnabledByDefault() {
+        let session = CaptureSession.current
+        XCTAssertTrue(session.autoScanEnabled)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+  
+    func testEditOrientationDownMirroedByDefault() {
+      let session = CaptureSession.current
+      XCTAssertEqual(session.editImageOrientation, CGImagePropertyOrientation.downMirrored)
     }
     
 }
