c26dd8f3606a2e1702ef7cb2336a2df53d71aaf8
Valeriy Van <github@w7software.com>
2018-08-11 15:43:01 +0200

    Simplifies isWithin func of extension on CIRectangleFeature

--- WeScan/Extensions/CIRectangleFeature+Utils.swift
+++ WeScan/Extensions/CIRectangleFeature+Utils.swift
@@ -23,28 +23,10 @@ extension CIRectangleFeature {
     ///   - rectangleFeature: The other rectangle to compare this instance with.
     /// - Returns: True if the given rectangle is within the given distance of this rectangle instance.
     func isWithin(_ distance: CGFloat, ofRectangleFeature rectangleFeature: CIRectangleFeature) -> Bool {
-        
-        let topLeftRect = topLeft.surroundingSquare(withSize: distance)
-        if !topLeftRect.contains(rectangleFeature.topLeft) {
-            return false
-        }
-        
-        let topRightRect = topRight.surroundingSquare(withSize: distance)
-        if !topRightRect.contains(rectangleFeature.topRight) {
-            return false
-        }
-        
-        let bottomRightRect = bottomRight.surroundingSquare(withSize: distance)
-        if !bottomRightRect.contains(rectangleFeature.bottomRight) {
-            return false
-        }
-
-        let bottomLeftRect = bottomLeft.surroundingSquare(withSize: distance)
-        if !bottomLeftRect.contains(rectangleFeature.bottomLeft) {
-            return false
-        }
-        
-        return true
+        return [\CIRectangleFeature.topLeft, \CIRectangleFeature.topRight,
+                \CIRectangleFeature.bottomLeft, \CIRectangleFeature.bottomRight]
+               .map { self[keyPath: $0].surroundingSquare(withSize: distance).contains(rectangleFeature[keyPath: $0]) }
+               .reduce(true, { $0 && $1})
     }
     
     override open var description: String {
