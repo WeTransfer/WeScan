de8ebd6e5835a535b761791797950509e47168cb
James Campbell <james@printtapp.com>
2018-09-08 14:57:30 +0100

    Outlining tests.

--- WeScanTests/CGAffineTransformTests.swift
+++ WeScanTests/CGAffineTransformTests.swift
@@ -9,27 +9,27 @@
 import XCTest
 
 class CGAffineTransformTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    
+//  /// Convenience function to easily get a scale `CGAffineTransform` instance.
+//  ///
+//  /// - Parameters:
+//  ///   - fromSize: The size that needs to be transformed to fit (aspect fill) in the other given size.
+//  ///   - toSize: The size that should be matched by the `fromSize` parameter.
+//  /// - Returns: The transform that will make the `fromSize` parameter fir (aspect fill) inside the `toSize` parameter.
+//  static func scaleTransform(forSize fromSize: CGSize, aspectFillInSize toSize: CGSize) -> CGAffineTransform {
+//    let scale = max(toSize.width / fromSize.width, toSize.height / fromSize.height)
+//    return CGAffineTransform(scaleX: scale, y: scale)
+//  }
+//  
+//  /// Convenience function to easily get a translate `CGAffineTransform` instance.
+//  ///
+//  /// - Parameters:
+//  ///   - fromRect: The rect which center needs to be translated to the center of the other passed in rect.
+//  ///   - toRect: The rect that should be matched.
+//  /// - Returns: The transform that will translate the center of the `fromRect` parameter to the center of the `toRect` parameter.
+//  static func translateTransform(fromCenterOfRect fromRect: CGRect, toCenterOfRect toRect: CGRect) -> CGAffineTransform {
+//    let translate = CGPoint(x: toRect.midX - fromRect.midX, y: toRect.midY - fromRect.midY)
+//    return CGAffineTransform(translationX: translate.x, y: translate.y)
+//  }
     
 }
--- WeScanTests/UIImageTests.swift
+++ WeScanTests/UIImageTests.swift
@@ -9,27 +9,64 @@
 import XCTest
 
 class UIImageTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    
+//  /// Returns the same image with a portrait orientation.
+//  func applyingPortraitOrientation() -> UIImage {
+//    switch imageOrientation {
+//    case .up:
+//      return rotated(by: Measurement(value: Double.pi, unit: .radians), options: []) ?? self
+//    case .down:
+//      return rotated(by: Measurement(value: Double.pi, unit: .radians), options: [.flipOnVerticalAxis, .flipOnHorizontalAxis]) ?? self
+//    case .left:
+//      return self
+//    case .right:
+//      return rotated(by: Measurement(value: Double.pi / 2.0, unit: .radians), options: []) ?? self
+//    default:
+//      return self
+//    }
+//  }
+//  
+//  /// Data structure to easily express rotation options.
+//  struct RotationOptions: OptionSet {
+//    let rawValue: Int
+//    
+//    static let flipOnVerticalAxis = RotationOptions(rawValue: 1)
+//    static let flipOnHorizontalAxis = RotationOptions(rawValue: 2)
+//  }
+//  
+//  /// Rotate the image by the given angle, and perform other transformations based on the passed in options.
+//  ///
+//  /// - Parameters:
+//  ///   - rotationAngle: The angle to rotate the image by.
+//  ///   - options: Options to apply to the image.
+//  /// - Returns: The new image rotated and optentially flipped (@see options).
+//  func rotated(by rotationAngle: Measurement<UnitAngle>, options: RotationOptions = []) -> UIImage? {
+//    guard let cgImage = self.cgImage else { return nil }
+//    
+//    let rotationInRadians = CGFloat(rotationAngle.converted(to: .radians).value)
+//    let transform = CGAffineTransform(rotationAngle: rotationInRadians)
+//    let cgImageSize = CGSize(width: cgImage.width, height: cgImage.height)
+//    var rect = CGRect(origin: .zero, size: cgImageSize).applying(transform)
+//    rect.origin = .zero
+//    
+//    let format = UIGraphicsImageRendererFormat()
+//    format.scale = 1
+//    
+//    let renderer = UIGraphicsImageRenderer(size: rect.size, format: format)
+//    
+//    let image = renderer.image { renderContext in
+//      renderContext.cgContext.translateBy(x: rect.midX, y: rect.midY)
+//      renderContext.cgContext.rotate(by: rotationInRadians)
+//      
+//      let x = options.contains(.flipOnVerticalAxis) ? -1.0 : 1.0
+//      let y = options.contains(.flipOnHorizontalAxis) ? 1.0 : -1.0
+//      renderContext.cgContext.scaleBy(x: CGFloat(x), y: CGFloat(y))
+//      
+//      let drawRect = CGRect(origin: CGPoint(x: -cgImageSize.width / 2.0, y: -cgImageSize.height / 2.0), size: cgImageSize)
+//      renderContext.cgContext.draw(cgImage, in: drawRect)
+//    }
+//    
+//    return image
+//  }
     
 }
