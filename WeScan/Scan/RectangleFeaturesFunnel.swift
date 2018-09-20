d6762a38b7d8d415d16f06cc116aa132847fa5b1
James Campbell <james@printtapp.com>
2018-09-20 22:05:03 +0100

    Revert old logic.

--- WeScan/Scan/RectangleFeaturesFunnel.swift
+++ WeScan/Scan/RectangleFeaturesFunnel.swift
@@ -164,14 +164,14 @@ final class RectangleFeaturesFunnel {
     private func updateRectangleMatches() {
         resetMatchingScores()
         
-        for currentRect in rectangles {
-            for rect in rectangles {
-                if currentRect.matches(rect.rectangleFeature, withThreshold: matchingThreshold) {
-                    currentRect.matchingScore += 1
-                    rect.matchingScore += 1
-                }
-            }
+      for (i, currentRect) in rectangles.enumerated() {
+        for (j, rect) in rectangles.enumerated() {
+          if j > i && currentRect.matches(rect.rectangleFeature, withThreshold: matchingThreshold) {
+            currentRect.matchingScore += 1
+            rect.matchingScore += 1
+          }
         }
+      }
     }
     
     /// Resets the matching score of all of the rectangles in the queue to 0
