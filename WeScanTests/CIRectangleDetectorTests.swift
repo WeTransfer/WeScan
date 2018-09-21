78917ea35d48af20fa89e9f9976764b0aff0f593
JG <jasper_greenham@icloud.com>
2018-09-21 16:38:09 +0800

    Fixed failing tests

--- WeScanTests/CIRectangleDetectorTests.swift
+++ WeScanTests/CIRectangleDetectorTests.swift
@@ -11,46 +11,47 @@ import XCTest
 @testable import WeScan
 
 class CIRectangleDetectorTests: FBSnapshotTestCase {
-  
+    
     override func setUp() {
-      super.setUp()
-      
-      recordMode = false
+        super.setUp()
+        
+        recordMode = false
     }
     
     func testCorrectlyDetectsAndReturnsQuadilateral() {
-      
-      let targetSize = CGSize(width: 50, height: 50)
-      
-      let containerLeyer =  CALayer()
-      containerLeyer.backgroundColor = UIColor.white.cgColor
-      containerLeyer.frame = CGRect(x: 0, y: 0, width: targetSize.width, height: targetSize.height)
-      containerLeyer.masksToBounds = true
-      
-      let targetLayer = CALayer()
-      targetLayer.backgroundColor = UIColor.black.cgColor
-      targetLayer.frame = containerLeyer.frame.insetBy(dx: 5, dy: 5)
-      
-      containerLeyer.addSublayer(targetLayer)
-      
-      UIGraphicsBeginImageContextWithOptions(targetSize, true, 1.0)
-      
-      containerLeyer.render(in: UIGraphicsGetCurrentContext()!)
-      
-      let image = UIGraphicsGetImageFromCurrentImageContext()!
-      UIGraphicsEndImageContext()
-      
-      let ciImage = CIImage(cgImage: image.cgImage!)
-      let quad = CIRectangleDetector.rectangle(forImage: ciImage)!
-      
-      let resultView = UIView(frame: containerLeyer.frame)
-      resultView.layer.addSublayer(containerLeyer)
-      
-      let quadView = QuadrilateralView(frame: resultView.bounds)
-      quadView.drawQuadrilateral(quad: quad, animated: false)
-      quadView.backgroundColor = UIColor.red
-      resultView.addSubview(quadView)
-      
-      FBSnapshotVerifyView(resultView)
+        
+        let targetSize = CGSize(width: 50, height: 50)
+        
+        let containerLeyer =  CALayer()
+        containerLeyer.backgroundColor = UIColor.white.cgColor
+        containerLeyer.frame = CGRect(x: 0, y: 0, width: targetSize.width, height: targetSize.height)
+        containerLeyer.masksToBounds = true
+        
+        let targetLayer = CALayer()
+        targetLayer.backgroundColor = UIColor.black.cgColor
+        targetLayer.frame = containerLeyer.frame.insetBy(dx: 5, dy: 5)
+        
+        containerLeyer.addSublayer(targetLayer)
+        
+        UIGraphicsBeginImageContextWithOptions(targetSize, true, 1.0)
+        
+        containerLeyer.render(in: UIGraphicsGetCurrentContext()!)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let ciImage = CIImage(cgImage: image.cgImage!)
+        let quad = CIRectangleDetector.rectangle(forImage: ciImage)!
+        
+        let resultView = UIView(frame: containerLeyer.frame)
+        resultView.layer.addSublayer(containerLeyer)
+        
+        let quadView = QuadrilateralView(frame: resultView.bounds)
+        quadView.drawQuadrilateral(quad: quad, animated: false)
+        quadView.backgroundColor = UIColor.red
+        resultView.addSubview(quadView)
+        
+        FBSnapshotVerifyView(resultView)
     }
+    
 }
