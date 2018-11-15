86e5bca4e8d03a83073e71c5d328ee516242d08e
Julian <justdotjs@icloud.com>
2018-11-15 11:08:11 +0800

    Use the built in "Done" button (auto localized)

--- WeScan/Review/ReviewViewController.swift
+++ WeScan/Review/ReviewViewController.swift
@@ -32,8 +32,7 @@ final class ReviewViewController: UIViewController {
     }()
     
     lazy private var doneButton: UIBarButtonItem = {
-        let title = NSLocalizedString("wescan.review.button.done", tableName: nil, bundle: Bundle(for: ReviewViewController.self), value: "Done", comment: "A generic done button")
-        let button = UIBarButtonItem(title: title, style: .done, target: self, action: #selector(finishScan))
+        let button = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(finishScan))
         button.tintColor = navigationController?.navigationBar.tintColor
         return button
     }()
