fc12070d9fe1b81e255f521d0bd0f80d59142620
Valeriy Van <github@w7software.com>
2019-01-16 12:57:48 +0100

    Revert "Added guard instead of force unwrap to please linter"
    This reverts commit 9a497e25813f49e3c91738ff023d13eec9f5bf08.

--- WeScan/Common/VisionRectangleDetector.swift
+++ WeScan/Common/VisionRectangleDetector.swift
@@ -32,8 +32,11 @@ struct VisionRectangleDetector {
                 
                 let quads: [Quadrilateral] = results.map(Quadrilateral.init)
 
-                let biggest = quads.biggest()! // swiftlint:disable:this force_unwrapping (excluded by isEmpty in guard)
-
+                guard let biggest = quads.biggest() else { // Can't fail because guard excluded empty array,
+                    completion(nil)                        // but strict linter rules force to add this else case
+                    return                                 // which will never be executed.
+                }
+                
                 let transform = CGAffineTransform.identity
                     .scaledBy(x: image.extent.size.width, y: image.extent.size.height)
               
