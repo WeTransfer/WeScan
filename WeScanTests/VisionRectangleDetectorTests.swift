e85ac93eed2943dd1189577e69b2dc352f295170
Max Cobb <maxxc@mac.com>
2019-03-12 08:43:18 +0000

    moved UIImage->pixelBuffer to extension

--- WeScanTests/VisionRectangleDetectorTests.swift
+++ WeScanTests/VisionRectangleDetectorTests.swift
@@ -16,36 +16,6 @@ final class VisionRectangleDetectorTests: FBSnapshotTestCase {
     super.setUp()
     recordMode = false
   }
-
-  /// Function gathered from [here](https://stackoverflow.com/questions/44462087/how-to-convert-a-uiimage-to-a-cvpixelbuffer) to convert UIImage to CVPixelBuffer
-  ///
-  /// - Parameter image: image to be converted
-  /// - Returns: new CVPixelBuffer
-  private func buffer(from image: UIImage) -> CVPixelBuffer? {
-    let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-    var pixelBuffer : CVPixelBuffer?
-    let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-    guard (status == kCVReturnSuccess) else {
-      return nil
-    }
-
-    CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-    let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-
-    let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-    let context = CGContext(data: pixelData, width: Int(image.size.width), height: Int(image.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
-
-    context?.translateBy(x: 0, y: image.size.height)
-    context?.scaleBy(x: 1.0, y: -1.0)
-
-    UIGraphicsPushContext(context!)
-    image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
-    UIGraphicsPopContext()
-    CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-
-    return pixelBuffer
-  }
-
   
   func testCorrectlyDetectsAndReturnsQuadilateral() {
 
@@ -117,7 +87,7 @@ final class VisionRectangleDetectorTests: FBSnapshotTestCase {
     UIGraphicsEndImageContext()
 
     let expectation = XCTestExpectation(description: "Detect rectangle on CVPixelBuffer")
-    if let pixelBuffer = self.buffer(from: image) {
+    if let pixelBuffer = image.pixelBuffer() {
       VisionRectangleDetector.rectangle(forPixelBuffer: pixelBuffer) { (quad) in
 
         DispatchQueue.main.async {
