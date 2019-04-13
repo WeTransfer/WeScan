4b183522cc5d5d229cc545a8bf3bb06d98cc3839
Julian Kahnert <mail@juliankahnert.de>
2019-04-13 11:58:56 +0200

    switch to CIPhotoEffectMono filter

--- WeScan/Extensions/CIImage+Utils.swift
+++ WeScan/Extensions/CIImage+Utils.swift
@@ -10,30 +10,15 @@ import CoreImage
 import Foundation
 
 extension CIImage {
-    /// Applies an AdaptiveThresholding filter to the image, which enhances the image and makes it completely gray scale
-    func applyingAdaptiveThreshold() -> UIImage? {
-        guard let colorKernel = CIColorKernel(source:
-            """
-            kernel vec4 color(__sample pixel, float inputEdgeO, float inputEdge1)
-            {
-                float luma = dot(pixel.rgb, vec3(0.2126, 0.7152, 0.0722));
-                float threshold = smoothstep(inputEdgeO, inputEdge1, luma);
-                return vec4(threshold, threshold, threshold, 1.0);
-            }
-            """
-            ) else { return nil }
-        
-        let firstInputEdge = 0.25
-        let secondInputEdge = 0.75
-        
-        let arguments: [Any] = [self, firstInputEdge, secondInputEdge]
-
-        guard let enhancedCIImage = colorKernel.apply(extent: self.extent, arguments: arguments) else { return nil }
-
-        if let cgImage = CIContext(options: nil).createCGImage(enhancedCIImage, from: enhancedCIImage.extent) {
+    /// Applies an PhotoEffectMono filter to the image, which enhances the image and makes it completely gray scale
+    func applyingPhotoEffectMono() -> UIImage? {
+        let context = CIContext(options: nil)
+        guard let currentFilter = CIFilter(name: "CIPhotoEffectMono") else { return nil }
+        currentFilter.setValue(self, forKey: kCIInputImageKey)
+        if let output = currentFilter.outputImage,
+            let cgImage = context.createCGImage(output, from: output.extent) {
             return UIImage(cgImage: cgImage)
-        } else {
-            return UIImage(ciImage: enhancedCIImage, scale: 1.0, orientation: .up)
         }
+        return nil
     }
 }
