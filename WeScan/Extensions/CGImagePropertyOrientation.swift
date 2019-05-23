ec18212617ab12b5197b2149792a442d69d277f8
Yang Chen <yang.chen@accolade.com>
2019-05-23 12:47:29 -0700

    Made the switch cases in newlines

--- WeScan/Extensions/CGImagePropertyOrientation.swift
+++ WeScan/Extensions/CGImagePropertyOrientation.swift
@@ -10,14 +10,22 @@ import Foundation
 extension CGImagePropertyOrientation {
     init(_ uiOrientation: UIImage.Orientation) {
         switch uiOrientation {
-        case .up: self = .up
-        case .upMirrored: self = .upMirrored
-        case .down: self = .down
-        case .downMirrored: self = .downMirrored
-        case .left: self = .left
-        case .leftMirrored: self = .leftMirrored
-        case .right: self = .right
-        case .rightMirrored: self = .rightMirrored
+        case .up:
+            self = .up
+        case .upMirrored:
+            self = .upMirrored
+        case .down:
+            self = .down
+        case .downMirrored:
+            self = .downMirrored
+        case .left:
+            self = .left
+        case .leftMirrored:
+            self = .leftMirrored
+        case .right:
+            self = .right
+        case .rightMirrored:
+            self = .rightMirrored
         @unknown default:
             assertionFailure("Unknow orientation, falling to default")
             self = .right
