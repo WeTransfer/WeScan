592f07f6a5ad69416b8e1f058f8fc1d4a1769037
Sean <sean.ooi@me.com>
2020-03-27 15:20:35 -0600

    lower session preset resolution

--- Sources/WeScan/Scan/CaptureSessionManager.swift
+++ Sources/WeScan/Scan/CaptureSessionManager.swift
@@ -79,7 +79,7 @@ final class CaptureSessionManager: NSObject, AVCaptureVideoDataOutputSampleBuffe
         }
 
         captureSession.beginConfiguration()
-        captureSession.sessionPreset = .hd4K3840x2160
+        captureSession.sessionPreset = .hd1920x1080
         
         photoOutput.isHighResolutionCaptureEnabled = true
 
