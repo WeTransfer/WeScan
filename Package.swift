7405dd4e91fb5ff5c00ed33203e6ab52c776d883
Antoine van der Lee <ajvanderlee@gmail.com>
2020-12-02 14:28:00 +0100

    Clean up folder structure for localisation, fix all tests, prepare for SPM

--- Package.swift
+++ Package.swift
@@ -3,9 +3,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "Coyote",
+    name: "WeScan",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
-        // dev .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]) // dev
+        // dev .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
+        .library(name: "WeScan", targets: ["WeScan"])
     ],
     dependencies: [
         // dev .package(url: "https://github.com/danger/swift", from: "3.0.0"),
@@ -15,6 +19,11 @@ let package = Package(
         // This is just an arbitrary Swift file in the app, that has
         // no dependencies outside of Foundation, the dependencies section
         // ensures that the library for Danger gets build also.
-        // dev .target(name: "DangerDependencies", dependencies: ["Danger", "WeTransferPRLinter"], path: "Submodules/WeTransfer-iOS-CI/Danger-Swift", sources: ["DangerFakeSource.swift"])
+        // dev .target(name: "DangerDependencies", dependencies: ["Danger", "WeTransferPRLinter"], path: "Submodules/WeTransfer-iOS-CI/Danger-Swift", sources: ["DangerFakeSource.swift"]),
+        .target(name: "WeScan",
+                path: "Sources",
+                resources: [
+
+                ])
     ]
 )
