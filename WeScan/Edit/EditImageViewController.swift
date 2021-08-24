f08aa1ed6fec71f5e9bcfdda75401cd55922e143
Bas Broek <BasThomas@users.noreply.github.com>
2021-08-24 16:04:41 +0100

    Change `class` to `AnyObject` for protocols (#315)
    This was emitting a warning.

--- WeScan/Edit/EditImageViewController.swift
+++ WeScan/Edit/EditImageViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVFoundation
 
 /// A protocol that your delegate object will get results of EditImageViewController.
-public protocol EditImageViewDelegate: class {
+public protocol EditImageViewDelegate: AnyObject {
     /// A method that your delegate object must implement to get cropped image.
     func cropped(image: UIImage)
 }
