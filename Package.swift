51324c62d8e7133a426c591ec39391b4b99191af
Antoine van der Lee <4329185+AvdLee@users.noreply.github.com>
2022-11-17 12:14:40 +0100

    Update Danger, improve fastlane

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // We're hiding dev, test, and danger dependencies with // dev to make sure they're not fetched by users of this package.
 import PackageDescription
 
@@ -9,18 +9,9 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        // dev .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
         .library(name: "WeScan", type: .static, targets: ["WeScan"])
     ],
-    dependencies: [
-        // dev .package(name: "danger-swift", url: "https://github.com/danger/swift", from: "3.0.0"),
-        // dev .package(name: "WeTransferPRLinter", path: "Submodules/WeTransfer-iOS-CI/Danger-Swift")
-    ],
     targets: [
-        // This is just an arbitrary Swift file in the app, that has
-        // no dependencies outside of Foundation, the dependencies section
-        // ensures that the library for Danger gets build also.
-        // dev .target(name: "DangerDependencies", dependencies: [.product(name: "Danger", package: "danger-swift"), "WeTransferPRLinter"], path: "Submodules/WeTransfer-iOS-CI/Danger-Swift", sources: ["DangerFakeSource.swift"]),
         .target(name: "WeScan",
                 path: "WeScan",
                 exclude: [
